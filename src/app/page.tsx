d0d4c59112a4e2e1c67b850db27eec5006ecf5da
Gaurav Mer <gouravmer22@gmail.com>
2024-03-31 20:46:26 +0530

    add meta data and svg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
 import MyQuestion from "@/components/myQuestion";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+
+  title: 'Question Bank',
+  description: 'Helping user to add and summerize their question and answer',
+  openGraph: {
+    title: 'Question Bank',
+    description: 'Helping user to add and summerize their question and answer',
+    images: ['/exam.svg', 'exam.svg'],
+
+  },
+}
 
 export default async function Home() {
   if (!process.env.NEXT_PUBLIC_MY_URL) {
